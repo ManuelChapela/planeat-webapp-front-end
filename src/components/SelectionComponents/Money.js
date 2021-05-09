caace64d0d4945bef0da0625b079acaa645e93e9
ChrisDevMode <74246991+ChrisDevMode@users.noreply.github.com>
2021-05-09 13:53:37 +0200

    Pantalla selección en construcción

---
+++ src/components/SelectionComponents/Money.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Money = () => {
+
+    
+    return (
+        <div>
+            <h1>MONEY</h1>
+        </div>
+    );
+};
---
+++ src/components/SelectionComponents/Time.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Time = () => {
+    return (
+        <div>
+            <h1>TIME</h1>
+        </div>
+    )
+}
