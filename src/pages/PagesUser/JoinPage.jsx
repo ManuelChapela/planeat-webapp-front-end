c3c138df64cb2dc5f9329a4d5712393a2c9e4ff9
ChrisDevMode <74246991+ChrisDevMode@users.noreply.github.com>
2021-05-09 18:56:18 +0200

    Página join con maquetado básico

--- src/pages/PagesUser/JoinPage.jsx
+++ src/pages/PagesUser/JoinPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Header } from './../../components/Header/Header';
-import logo from './../../assets/logo.png';
+import { HeaderJoin } from './../../components/HeaderJoin/HeaderJoin';
+import logoSmall from './../../assets/mainIcon__small.svg';
 import { useHistory } from 'react-router';
 import { BtnMainIcons } from './../../components/BtnMainIcons/BtnMainIcons';
+
 // import { EditPass } from '../../components/EditPass/EditPass';
 import { InputMod } from '../../components/InputMod/InputMod';
+
+//Assets
+import btnGoogle from './../../assets/btn__google.svg';
+import btnLogin from './../../assets/btn__log-in.svg';
+import btnSignup from './../../assets/btn__sign-up.svg';
 
 
 
@@ -22,17 +28,17 @@ export const JoinPage = () => {
         <div className='container'>
 
             <header>
-                <Header logo={logo} text="¿Quieres añadir una receta a favoritos o eliminar una receta de tus sugerencias?" />
+                <HeaderJoin cssClass='logo__join' logo={logoSmall} text="¿Quieres añadir una receta a favoritos o eliminar una receta de tus sugerencias?" />
             </header>
 
             <main>
-                <h2>Únete a X</h2>
-                <h3>Aprovecha lo que hay en tu nevera con recetas rápidas y sencillas ¡Únete ahora!</h3>
+                <h2 className='join__cta'>Únete a Planeat</h2>
+                <h3 className='join__text'>Aprovecha lo que hay en tu nevera con recetas rápidas y sencillas ¡Únete ahora!</h3>
 
-                <div className='btn__box'>
-                    <button className="btnGoogle" onClick={handleClickGoogle}>Continúa con Google</button>
-                    <button className="btnLogIn" onClick={handleClickLogIn}>Iniciar sesión</button>
-                    <button className="btnSignUp" onClick={handleClickSignUp}>Regístrate</button>
+                <div className='btn__join-box'>
+                    <img src={btnGoogle} className="btn__google" onClick={handleClickGoogle} alt="botón de login con google" />
+                    <img src={btnLogin} className="btn__log-in" onClick={handleClickLogIn} alt="botón de login con google" />
+                    <img src={btnSignup} className="btn__sign-up" onClick={handleClickSignUp} alt="botón de login con google" />
                 </div>
 
             </main>
