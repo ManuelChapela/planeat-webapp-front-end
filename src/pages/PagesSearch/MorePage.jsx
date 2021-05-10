1548eee8accbb76b89b84cda3c898a4c1cebbc54
ManuelChapela <manuelchapelap@gmail.com>
2021-05-10 20:33:14 +0200

    ADD skip function

--- src/pages/PagesSearch/MorePage.jsx
+++ src/pages/PagesSearch/MorePage.jsx
@@ -1,9 +1,13 @@
 import React, {useContext} from 'react';
 import LoggedContext from './../../context/loggedContext';
+import PrefsContext from './../../context/prefsContext';
+
 
 // Assets
-import logo from './../../assets/logo.png';
+// import logo from './../../assets/logo.png';
 import btnNext from './../../assets/btnNext.svg';
+import backArrow from './../../assets/back__arrow.svg';
+
 
 // Hooks
 import { useHistory } from 'react-router';
@@ -12,34 +16,27 @@ import { useHistory } from 'react-router';
 import { HeaderNoLogo } from './../../components/HeaderNoLogo/HeaderNoLogo';
 import { Painter } from '../../components/Painter/Painter';
 import { BtnNext } from '../../components/BtnNext/BtnNext';
-import { BtnBack } from '../../components/BtnBack/BtnBack';
-import { BtnSkip } from '../../components/BtnSkip/BtnSkip';
 import { BtnMainIcons } from '../../components/BtnMainIcons/BtnMainIcons';
+import { NavBar } from '../../components/NavBar/NavBar';
 
 export const MorePage = () => {
 
     const {logged, setLogged} = useContext(LoggedContext);
+    const { prefs, setPrefs } = useContext(PrefsContext);
 
     let history = useHistory();
     const handleClick = () => history.push("/seleccion");
     const handleBack = () => history.push("/noquiero");
+    const handleSkip = () => {
+            setPrefs({...prefs, bannedCategories: []})
+            history.push('/seleccion')}
 
     return (
         <div className='container'>
             <header className='header__not'>
 
-                <div className="btn__box">
-
-                    <BtnBack 
-                        text="Volver"  
-                        action={handleBack}
-                    />
-
-                    <BtnSkip 
-                        text="Saltar"
-                        action={handleClick}
-                    />              
-
+                <div className='nav__bar-box'>
+                     <NavBar cssClass='back__arrow' actionBack={handleBack} actionNext={handleSkip} backArrow={backArrow} />
                 </div>
 
                 <HeaderNoLogo text='¿Qué ingrediente no quieres usar?'/>
