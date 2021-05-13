d8abee90c999e786b3da9b0410b6894e30c9329c
ChrisDevMode <74246991+ChrisDevMode@users.noreply.github.com>
2021-05-13 18:24:39 +0200

    Funcionalidad página receta detalle

--- src/components/BtnLikeDislike/BtnBanned.js
+++ src/components/BtnLikeDislike/BtnBanned.js
@@ -4,17 +4,21 @@ import React, {useState} from 'react';
 import ThumbDownAltIcon from '@material-ui/icons/ThumbDownAlt';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 
-export const BtnBanned = ({action, cssClass, icon, stateBanned}) => {
+export const BtnBanned = ({action, cssClass, iconHandUp, stateBanned}) => {
     
     
 
     return (
+
         <div className="btn__back-box">
-
-            <div onClick={action} className={cssClass} type="button" > { stateBanned ? <ThumbUpIcon /> :  <ThumbDownAltIcon />} </div>
-
+            <img 
+                onClick={action} 
+                className={cssClass} 
+                src={ iconHandUp } 
+                alt="icono favoritos" 
+                
+                />
         </div>
 
     )
-        
 }
