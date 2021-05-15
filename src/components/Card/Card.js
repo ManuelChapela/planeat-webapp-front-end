cffde4d4ad0f15a09e7c8668f1158dfc91016274
rovilram <robertovillaresrama@gmail.com>
2021-05-15 19:31:14 +0200

    fix ban ingredient button functionality

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,17 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Chip from "@material-ui/core/Chip";
+import Chip from '@material-ui/core/Chip';
 
 import { useHistory } from 'react-router';
 
-
-// Assets 
+// Assets
 import btnReceta from './../../assets/btnReceta.svg';
 import iconHeart from './../../assets/icon__heart.svg';
 import iconHeartFill from './../../assets/icon__heart-fill.svg';
 import iconHand from './../../assets/icon__down-hand.svg';
 
 // Componentes
-import {BtnLikeDislike} from '../../components/BtnLikeDislike/BtnLikeDislike';
+import { BtnLikeDislike } from '../../components/BtnLikeDislike/BtnLikeDislike';
 import { BtnBanned } from '../../components/BtnLikeDislike/BtnBanned';
 
 // Context
@@ -23,186 +22,173 @@ import HistoryContext from './../../context/historyContext';
 import useLocalStorage from './../../Hooks/useLocalStorage';
 import useFetch from './../../Hooks/useFetch';
 
+export const Card = ({ item, action, recipesState }) => {
+  const { recipes, setRecipes } = recipesState;
 
-export const Card = ({item, action, recipesState}) => {
-    const {recipes, setRecipes} = recipesState;
+  const handleFav = () => {
+    const logged = true;
+    return logged ? handleLikeState() : handleClickJoin();
+  };
+  const handleBan = () => {
+    return logged ? handleBannedState() : handleClickJoin();
+  };
 
-    const handleFav = () => {
-        const logged = true;
-        return logged ? handleLikeState() : handleClickJoin();
+  let history = useHistory();
+  let location = useLocation();
+
+  const [recipe, setRecipe] = useState({ item });
+
+  //Fetch
+  const [fetchStateFav, fetchDataFav] = useFetch();
+  const [fetchStateNoFav, fetchDataNoFav] = useFetch();
+
+  const { logged, setLogged } = useContext(LoggedContext);
+  const [token, setToken] = useLocalStorage('token', '');
+  const { currentUrl, setCurrentUrl } = useContext(HistoryContext);
+
+  const printIngredients = (item) => {
+    const newItems = item.ingredients
+      .filter((ing, i) => {
+        return i < 6;
+      })
+      .map((ing) => {
+        return (
+          <Chip
+            className="sticker"
+            style={{
+              backgroundColor: '#EFFFD9',
+              color: '#266A29',
+              border: '1px solid #8BC53F',
+            }}
+            label={ing}
+            //   className={classes.chip}
+          />
+        );
+      });
+
+    return newItems;
+  };
+
+  // Funcionalidad botones
+  const handleLikeState = () => {
+    const method = 'POST';
+    const headers = { Authorization: `Bearer ${token}` };
+    const body = { idRecipe: item.id };
+
+    if (recipe.fav) {
+      const url = `${process.env.REACT_APP_BACKEND_URL}/user/delfav`;
+      fetchDataFav({ url, method, headers, body });
+    } else {
+      const url = `${process.env.REACT_APP_BACKEND_URL}/user/addfav`;
+      fetchDataFav({ url, method, headers, body });
     }
-    const handleBan = () => {
-        return logged ? handleBannedState() : handleClickJoin();
+  };
+  const handleClickJoin = () => {
+    setCurrentUrl({ currentUrl: '/nevera' });
+    history.push('/join');
+  };
+
+  const handleBannedState = () => {
+    const method = 'POST';
+    const headers = { Authorization: `Bearer ${token}` };
+    const body = { idRecipe: item.id };
+    console.log('BANEADO???', recipe.ban);
+    if (recipe.ban) {
+      const url = `${process.env.REACT_APP_BACKEND_URL}/user/delnofav`;
+      fetchDataNoFav({ url, method, headers, body });
+    } else {
+      const url = `${process.env.REACT_APP_BACKEND_URL}/user/addnofav`;
+      fetchDataNoFav({ url, method, headers, body });
     }
+  };
 
+  // UseEffects
+  useEffect(() => {
+    console.log(recipe);
+    fetchStateFav.isSuccess &&
+      fetchStateFav.data.OK &&
+      setRecipe({ ...recipe, fav: !recipe.fav });
+  }, [fetchStateFav]);
 
-    let history = useHistory();
-    let location = useLocation();
+  useEffect(() => {
+    fetchStateNoFav.isSuccess && fetchStateNoFav.data.OK && delBannedRecipe();
+  }, [fetchStateNoFav]);
 
-    const [recipe, setRecipe] = useState({item});
-    
+  const delBannedRecipe = () => {
+    console.log('RECETA A BORRAR', recipe.item.id);
+    console.log('RECETAS EN ESTADO', recipes);
+    const newRecipes = recipes.filter((el) => el.id !== recipe.item.id);
+    console.log('NEW RECIPES', newRecipes);
+    setRecipes(newRecipes);
+  };
 
-    //Fetch
-    const [fetchStateFav, fetchDataFav] = useFetch();
-    const [fetchStateNoFav, fetchDataNoFav] = useFetch();
+  return (
+    <div className="card">
+      <div className="card__img-box">
+        <img className="card__img" src={item.img} alt="" />
 
-    const { logged, setLogged } = useContext(LoggedContext);
-    const [token, setToken] = useLocalStorage('token', '');
-    const { currentUrl, setCurrentUrl } = useContext(HistoryContext);
-
-    const printIngredients = (item) => {
-        const newItems = item.ingredients.filter( (ing, i) => {
-            return i < 6
-        }).map( ing => {
-            return <Chip
-                className='sticker'
-                style={ {
-                    backgroundColor: '#EFFFD9',
-                    color: '#266A29',
-                    border: '1px solid #8BC53F'
-                } }
-                label={ing}
-                //   className={classes.chip}
-                />
-        })
-
-        return newItems;
-    }
-
-    // Funcionalidad botones
-    const handleLikeState = () => {
-        const method = 'POST';
-        const headers = { Authorization: `Bearer ${token}` };
-        const body = { idRecipe: item.id };
-
-        if (recipe.fav) {
-          const url = `${process.env.REACT_APP_BACKEND_URL}/user/delfav`;
-          fetchDataFav({ url, method, headers, body });
-
-        } else {
-          const url = `${process.env.REACT_APP_BACKEND_URL}/user/addfav`;
-          fetchDataFav({ url, method, headers, body });
-        }
-
-    };
-    const handleClickJoin = () => {
-        setCurrentUrl({ currentUrl: '/nevera' });
-        history.push('/join');
-    };
-
-    const handleBannedState = () => {
-        const method = 'POST';
-        const headers = { Authorization: `Bearer ${token}` };
-        const body = { idRecipe: item.id };
-        console.log('BANEADO???', recipe.ban);
-        if (recipe.ban) {
-          const url = `${process.env.REACT_APP_BACKEND_URL}/user/delnofav`;
-          fetchDataNoFav({ url, method, headers, body });
-        } else {
-          const url = `${process.env.REACT_APP_BACKEND_URL}/user/addnofav`;
-          fetchDataNoFav({ url, method, headers, body });
-        }
-      };
-
-
-      // UseEffects
-      useEffect(() => {
-          console.log(recipe);
-        fetchStateFav.isSuccess &&
-          fetchStateFav.data.OK &&
-          setRecipe({ ...recipe, fav: !recipe.fav });
-      }, [fetchStateFav]);
-
-      useEffect(() => {
-        fetchStateNoFav.isSuccess &&
-          fetchStateNoFav.data.OK &&
-          console.log(recipes);
-          setRecipes([ ...recipes.filter( (el) => 
-              el.id !== recipe.id
-          )]);
-      }, [fetchStateNoFav]);
-
-
-
-
-
-    return (
-        <div className="card">
-            <div className="card__img-box">
-                <img className='card__img' src={item.img} alt="" />
-
-                <div className="card__title-box">
-                    <h3>{item.mainTitle}</h3>
-                </div>
-
-                <div className="card__icon-box">
-
-                    <div onClick={handleFav} className="testing">
-                        <BtnLikeDislike
-                            // action={logged ? handleLikeState : handleClickJoin}
-                            stateLike={recipe.fav}
-                            iconHeart={iconHeart}
-                            iconHeartFill={iconHeartFill}
-                            cssClass="icon__heart"
-                            alt="icono corazón"
-                        />
-                    </div>
-
-                    <div onClick={handleBan} className="testing"> 
-                        <BtnBanned
-                            // action={logged ? handleBannedState : handleClickJoin}
-                            stateBanned={recipe.ban}
-                            iconHandUp={iconHand}
-                            className="icon__down-hand"
-                            alt="icono mano hacia abajo"
-                        />
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="card__title-des">
-                <div className="card__title-des--box">
-                    <h2>{item.title}</h2>
-                    <h4>{item.type}</h4>
-                </div>
-                <hr />
-            </div>
-
-            <div className="card__ingredients-box">
-
-                <div className="card__detail-text--box">
-                    <div className="card__preferences">
-                        <p>{item.price}</p>
-                        <small>/</small>
-                        <p>{item.time + ' minutos'}</p>
-                    </div>
-
-                    <p className='card__ing-text'>Ingredientes</p>
-                    <div className="card__ingredients">
-                        <div className="ingredients__stickers-box">
-                            {printIngredients(item)}
-                           
-                           
-                            
-                        </div>
-                            
-                           
-                        
-                        <div onClick={()=>action(item.id)} className="card__button-box">
-                            <img className='card__button' onClick={() => console.log('Click en botón')} src={btnReceta} alt="botón ver receta" />
-                            {/* <button className='card__button'>Ver Receta</button> */}
-                        </div>
-
-                    </div>
-                    
-
-
-                </div>
-
-            </div>
-
+        <div className="card__title-box">
+          <h3>{item.mainTitle}</h3>
         </div>
-    )
-}
+
+        <div className="card__icon-box">
+          <div onClick={handleFav} className="testing">
+            <BtnLikeDislike
+              // action={logged ? handleLikeState : handleClickJoin}
+              stateLike={recipe.fav}
+              iconHeart={iconHeart}
+              iconHeartFill={iconHeartFill}
+              cssClass="icon__heart"
+              alt="icono corazón"
+            />
+          </div>
+
+          <div onClick={handleBan} className="testing">
+            <BtnBanned
+              // action={logged ? handleBannedState : handleClickJoin}
+              stateBanned={recipe.ban}
+              iconHandUp={iconHand}
+              className="icon__down-hand"
+              alt="icono mano hacia abajo"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="card__title-des">
+        <div className="card__title-des--box">
+          <h2>{item.title}</h2>
+          <h4>{item.type}</h4>
+        </div>
+        <hr />
+      </div>
+
+      <div className="card__ingredients-box">
+        <div className="card__detail-text--box">
+          <div className="card__preferences">
+            <p>{item.price}</p>
+            <small>/</small>
+            <p>{item.time + ' minutos'}</p>
+          </div>
+
+          <p className="card__ing-text">Ingredientes</p>
+          <div className="card__ingredients">
+            <div className="ingredients__stickers-box">
+              {printIngredients(item)}
+            </div>
+
+            <div onClick={() => action(item.id)} className="card__button-box">
+              <img
+                className="card__button"
+                onClick={() => console.log('Click en botón')}
+                src={btnReceta}
+                alt="botón ver receta"
+              />
+              {/* <button className='card__button'>Ver Receta</button> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
