f9b961dbdfe7db803c54ae992d94526722faf4d6
rovilram <robertovillaresrama@gmail.com>
2021-05-17 11:48:42 +0200

    style botton buttons

--- src/components/BtnMainIcons/BtnUser/BtnUser.js
+++ src/components/BtnMainIcons/BtnUser/BtnUser.js
@@ -5,22 +5,19 @@ import iconUserOn from './../../../assets/icon__userOn.svg';
 
 import { useLocation } from 'react-router-dom';
 
+export const BtnUser = ({ action }) => {
+  const location = useLocation();
 
+  console.log('BTN USER', location.pathname);
 
-export const BtnUser = ( {action} ) => {
+  return (
+    <div className="icon__user" onClick={action}>
+      {location.pathname === '/profile' ? <img src={iconUserOn} alt="icono lupa" /> :
+       location.pathname === '/profile-noquiero' ? <img src={iconUserOn} alt="icono lupa" /> : 
+       location.pathname === '/more-profile' ? <img src={iconUserOn} alt="icono lupa" /> :
+        <img src={iconUser} alt="icono lupa" />}
 
-    const location = useLocation();
-    
-    console.log("BTN USER", location.pathname);
-
-    return (
-        <div className='icon__user' onClick={action}>
-            {
-            location.pathname === '/profile' ? <img src={iconUserOn} alt="icono lupa" />
-            : <img src={iconUser} alt="icono lupa" />
-            }
-
-            {/* <img src={iconUser} alt="Icono de usuario"/> */}
-        </div>
-    );
+      {/* <img src={iconUser} alt="Icono de usuario"/> */}
+    </div>
+  );
 };
